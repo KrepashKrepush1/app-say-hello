a34ac8e9cad37ed2b1d77ed032ee74a3fdf25dee
KrepashKrepush1 <diobusiness6669@gmail.com>
2026-05-29 17:50:05 +0700

    29 may tambahan

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KrepashKrepush1/app-say-hello
 
 go 1.26.3
+
+require github.com/KrepashKrepush1/go-say-hello v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
-
-
+import ("fmt"
+go_say_hello "github.com/KrepashKrepush1/go-say-hello"// pake go get di materi nomor 85 bab 2
+)
 
 func main() {
-	
+	fmt.Println(go_say_hello.SayHello())
 }
